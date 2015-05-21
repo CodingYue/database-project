eb493d4bfaaed78bce1d9c3729fe8817ec31040e
CodingYue <this.isssssyy@gmail.com>
2015-05-21 23:36:40 +0100

    ..

--- src/bookstore/Ordering.java
+++ src/bookstore/Ordering.java
@@ -3,13 +3,12 @@ package bookstore;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Scanner;
 
 /**
  * Created by yy on 5/14/15.
  */
 public class Ordering {
-    double ordering(Connection con, String[] ISBN) throws Exception {
+    double ask_price(Connection con, String[] ISBN, int[] amount) throws Exception {
 
         double total = 0;
 
@@ -18,11 +17,35 @@ public class Ordering {
             PreparedStatement query = con.prepareStatement("SELECT * from Book Where Book.ISBN = " + ISBN[i]);
             ResultSet result = query.executeQuery();
             if (result == null) continue;
-            total += result.getDouble("price");
+            total += result.getDouble("price") * amount[i];
 
             result.close();
         }
 
         return total;
+    }
+    void ordering(Connection con, String[] args) throws Exception {
+
+        PreparedStatement query = con.prepareStatement("SELECT * FROM Orders WHERE " +
+                "ISBN = " + args[0] + " login_name = " + args[1] + " made_date = " + args[2]);
+
+        ResultSet result = query.executeQuery();
+
+        if (result == null) {
+            String tmpStatement = "INSERT INTO Orders VALUES(";
+            for (int i = 0; i < args.length; ++i) {
+                tmpStatement += args[i];
+                if (i+1 < args.length) tmpStatement += ", ";
+                else tmpStatement += ")";
+            }
+            PreparedStatement add = con.prepareStatement(tmpStatement);
+            add.executeUpdate();
+        } else {
+            String tmpStatement = "UPDATE Orders SET amount = amount + " + args[3] + " WHERE " +
+                    "ISBN = " + args[0] + " login_name = " + args[1] + " made_date = " + args[2];
+            PreparedStatement update = con.prepareCall(tmpStatement);
+            update.executeUpdate();
+        }
+
     }
 }
